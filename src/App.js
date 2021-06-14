6df50ec3f037d54db89f163d7c2c0ba8f4a42947
nlok5923 <nlokhande5923@gmail.com>
2021-06-14 23:42:11 +0530

    add % meter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@
 import './App.css'
 import Card from './components/Card/index'
 import { React, useState, useEffect } from 'react'
+import {
+  CircularProgressbar,
+  buildStyles
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+// Animation
+import { easeQuadInOut } from "d3-ease";
+import AnimatedProgressProvider from './AnimationProgessProvider';
+
 
 const App = () => {
 
   const [ days, setDays ] = useState([]);
   const [ passed, setPassed ] = useState(0);
+  const [ passedPercentage, setPasspercentage ] = useState(0);
+
   useEffect(() => {
     var birthDay = new Date("06/14/2001");
     var currentDay = new Date();
@@ -19,11 +31,36 @@ const App = () => {
     var diffInTime = lastDay.getTime() - birthDay.getTime();
     var diffInDays = diffInTime / (1000 * 3600 * 24 * 7);
     console.log(diffInDays);
+    let percentage = passedTime / diffInDays;
+    percentage = percentage * 100;
+    percentage = Math.floor(percentage);
+    setPasspercentage(percentage);
     setDays([...Array(Math.floor(diffInDays)).keys()]);
   },[])
   return (
     <div>
-    <div className="head"></div>
+    <div className="head">
+    <AnimatedProgressProvider
+        valueStart={0}
+        valueEnd={passedPercentage}
+        duration={1.4}
+        easingFunction={easeQuadInOut}
+      >
+        {value => {
+          const roundedValue = Math.round(value);
+          return (
+            <CircularProgressbar
+              value={value}
+              text={`${roundedValue}%`}
+              styles={buildStyles({ pathTransition: "none",
+              trailColor: 'rgba(140, 7, 142, 0.5)',
+              backgroundColor: 'rgba(140, 7, 142, 0.5)',
+            })}
+            />
+          );
+        }}
+      </AnimatedProgressProvider>
+    </div>
     <div className="calender-wrapper">
       <div className="card-wrapper">
         {days.map((data, index) =>  ( data < passed ) ? <Card number={data} passed={true} /> : <Card number={data} passed={false} /> )}
